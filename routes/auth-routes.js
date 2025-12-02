b33fef071a5964b7b172b99eabbbadd15379d1c6
Christopher Adegoke <christopheradegoke4@gmail.com>
2025-12-02 14:34:07 +0100

    modified some errors

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,7 +2,7 @@ import express from "express"
 const router = express.Router()
 import { registerUser, loginUser, changePassword, forgottenPassword } from "../controllers/auth-controller.js"
 import { authMiddleware } from "../middleware/auth-middleware.js"
-router.post("/regsiter", registerUser)
+router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.post("/change-password", authMiddleware, changePassword)
 // router.post("/forgotten-password", authMiddleware, forgottenPassword)
