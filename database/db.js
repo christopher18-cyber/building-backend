d4bcdc7d07f862d6b6ad506818ae655d6b4b38c7
Christopher Adegoke <christopheradegoke4@gmail.com>
2025-12-02 06:23:03 +0100

    modified some error

--- database/db.js
+++ database/db.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
-const connectToDB = async () => {
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI;
+
+async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("MONGOBD connected successfully.")
-    }
-    catch (err) {
-        console.error("MongoDB connection failed")
-        process.exit(1)
+        await mongoose.connect(uri, {
+            dbName: "myDatabase", // optional if included in URI
+        });
+        console.log("MongoDB connected successfully");
+    } catch (err) {
+        console.error("MongoDB connection failed", err);
     }
 }
+
+
 
 export default connectToDB
